93e59449d85739ff02bb3f8dfc553e5f7ac39588
Wilian Krinke <krinkewilian@gmail.com>
2022-02-24 00:26:43 -0300

    inserindo outros verbos http

--- modulo1/my-bank-api/routes/account/index.js
+++ modulo1/my-bank-api/routes/account/index.js
@@ -4,10 +4,12 @@ import { promises as fs } from 'fs';
 const {readFile, writeFile} = fs
 const router = express.Router()
 
+const accountJsonFileName = 'accounts.json'
+
 router.post('/', async (req, res) => {
     try {
         let accountReceived = req.body;    
-        const data = JSON.parse(await readFile('accounts.json'));
+        const data = JSON.parse(await readFile(accountJsonFileName));
 
         accountReceived = {
             id: data.nextId++,
@@ -15,9 +17,42 @@ router.post('/', async (req, res) => {
         }
              
         data.accounts.push(accountReceived);    
-        await writeFile('accounts.json', JSON.stringify(data, null, 2));
+        await writeFile(accountJsonFileName, JSON.stringify(data, null, 2));
 
         res.status(200).send('Account created: ' + JSON.stringify(accountReceived));       
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.get('/:id?', async (req, res) => {
+    try {
+        const {id} = req.params
+
+        if (id !== undefined) { 
+            const {accounts} = JSON.parse(await readFile(accountJsonFileName));
+            const singleData = accounts.filter((item) => item.id === parseInt(id))
+         
+            res.status(200).send(JSON.stringify(singleData))            
+        } else {
+            const {accounts} = JSON.parse(await readFile(accountJsonFileName));
+            res.status(200).send(JSON.stringify(accounts))
+        }        
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+
+        const data = JSON.parse(await readFile(accountJsonFileName));
+        data.accounts = data.accounts.filter(item => item.id !== parseInt(id));
+
+        await writeFile(accountJsonFileName, JSON.stringify(data, null, 2));
+
+        res.status(200).send('Deleted');
     } catch (error) {
         res.status(400).send(error.message)
     }
