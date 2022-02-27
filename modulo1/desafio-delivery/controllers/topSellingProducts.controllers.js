6dd2a65c5066676263a987dbfc90115ad95852e9
Wilian Krinke <krinkewilian@gmail.com>
2022-02-27 17:58:53 -0300

    finalização de endpoint top sellings

--- modulo1/desafio-delivery/controllers/topSellingProducts.controllers.js
+++ modulo1/desafio-delivery/controllers/topSellingProducts.controllers.js
@@ -3,7 +3,7 @@ import { getTopSellingProductsServices } from "../services/getTopSellingProducts
 export async function topSellingProductsController(req, res, next){
     try {
         const moreSellingProducts = await getTopSellingProductsServices()
-        res.status(200).send('Okay')
+        res.status(200).send(JSON.stringify(moreSellingProducts))
     } catch (error) {
         console.log(error.message)
         res.status(400).send(error.message)
--- modulo1/desafio-delivery/services/getTopSellingProducts.services.js
+++ modulo1/desafio-delivery/services/getTopSellingProducts.services.js
@@ -4,23 +4,46 @@ export async function getTopSellingProductsServices(){
     const allDatas = await getAllDatasJson()
     const arrayPedidos = allDatas.pedidos.filter(item => item.entregue === true)
 
-    //const arrayPedidos1 = arrayPedidos.filter(item => item.produto === "Pizza a Moda")
-    // const arrayPedidos2 = arrayPedidos.filter(item => item.produto === "Pizza Atum")
-    // const arrayPedidos3 = arrayPedidos.filter(item => item.produto === "Pizza Calabresa")
-    // const arrayPedidos4 = arrayPedidos.filter(item => item.produto === "Pizza Frango com Catupiry")
-    // const arrayPedidos5 = arrayPedidos.filter(item => item.produto === "Pizza Muçarela")
-    // const arrayPedidos6 = arrayPedidos.filter(item => item.produto === "Pizza Napolitana")
-    // const arrayPedidos7 = arrayPedidos.filter(item => item.produto === "Pizza Pepperoni")
+    const arrayPedidos1 = arrayPedidos.filter(item => item.produto === "Pizza a Moda")
+    const arrayPedidos2 = arrayPedidos.filter(item => item.produto === "Pizza Atum")
+    const arrayPedidos3 = arrayPedidos.filter(item => item.produto === "Pizza Calabresa")
+    const arrayPedidos4 = arrayPedidos.filter(item => item.produto === "Pizza Frango com Catupiry")
+    const arrayPedidos5 = arrayPedidos.filter(item => item.produto === "Pizza Muçarela")
+    const arrayPedidos6 = arrayPedidos.filter(item => item.produto === "Pizza Napolitana")
+    const arrayPedidos7 = arrayPedidos.filter(item => item.produto === "Pizza Pepperoni")
 
-    // const object1 = {
-    //     Pizza: "Pizza a Moda",
-    //     qtd: arrayPedidos1.length
-    // }
+    const arrayTopOrders = [
+        {
+            Pizza: "Pizza a Moda",
+            qtd: arrayPedidos1.length
+        },
+        {
+            Pizza: "Pizza Atum",
+            qtd: arrayPedidos2.length
+        },
+        {
+            Pizza: "Pizza Calabresa",
+            qtd: arrayPedidos3.length
+        },
+        {
+            Pizza: "Pizza Frango com Catupiry",
+            qtd: arrayPedidos4.length
+        },
+        {
+            Pizza: "Pizza Muçarela",
+            qtd: arrayPedidos5.length
+        },
+        {
+            Pizza: "Pizza Napolitana",
+            qtd: arrayPedidos6.length
+        },
+        {
+            Pizza: "Pizza Pepperoni",
+            qtd: arrayPedidos7.length
+        },
+    ]
 
-    // console.log(object1)
-    // //Achar outra maneira
-    
-    
+    arrayTopOrders.sort((a,b) => b.qtd - a.qtd)
 
-    
+    return arrayTopOrders;
 }
