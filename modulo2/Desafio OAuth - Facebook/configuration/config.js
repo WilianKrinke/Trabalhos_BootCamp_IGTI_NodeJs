f6e396a2d99de751110ea01106274589f99c6786
Wilian Krinke <krinkewilian@gmail.com>
2022-03-17 20:29:34 -0300

    inserindo oauthfacebook

---
+++ modulo2/Desafio OAuth - Facebook/configuration/config.js
@@ -0,0 +1,5 @@
+module.exports={
+    "api_key": "234720918761-rfle8pmhsgictgnhg232cvgnciv6itno.apps.googleusercontent.com",
+    "api_secret": "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",
+    "callback_url": "http://localhost:3000/auth/google/callback"
+}
---
+++ modulo2/Desafio OAuth - Facebook/configuration/configFacebook.js
@@ -0,0 +1,5 @@
+module.exports={
+    "api_key": "753591282313750",
+    "api_secret": "cdb80937a8a8ca343020ee025ba6fec0",
+    "callback_url": "http://localhost:3000/auth/facebook/callback"
+}
