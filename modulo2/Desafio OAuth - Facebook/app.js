158dcf5110821fdc8c5f882b495da9de62afcb33
Wilian Krinke <krinkewilian@gmail.com>
2022-03-17 21:33:11 -0300

    alteração feita com sucesso

--- modulo2/Desafio OAuth - Facebook/app.js
+++ modulo2/Desafio OAuth - Facebook/app.js
@@ -53,8 +53,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
-
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['profile'] }));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 
 app.get('/auth/google/callback', 
   passport.authenticate('google',  { successRedirect : '/', failureRedirect: '/login' }),
